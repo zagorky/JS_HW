cf7698169fc8f5fba0cabb5fcec468a892c37ad0
zagorky <zagorkiynahyi@gmail.com>
2024-04-04 18:48:42 +0300

    изменения

--- HW DOM/JS/2.js
+++ HW DOM/JS/2.js
@@ -3,17 +3,17 @@
 // «Вперед» и «Назад», чтобы можно было листать 
 // картинки. Изображения должны листаться «по кругу».
 
-let count = 0;
+let count = 1;
 document.querySelector(".back").onclick = function (){
     count--;
-    if (count<1) count = 3;
+    if (count<1) count = 6;
     loadImg (count);
 }
 document.querySelector(".forward").onclick = function (){
         count++;
-        if (count>3) count = 1;
+        if (count>6) count = 1;
         loadImg (count);
     }
     function loadImg(count){
-        document.querySelector("img").src=`img/js${count}.jpg`;
+        document.querySelector("img").src=`/HW DOM//img/${count}.jpg`;
     }
