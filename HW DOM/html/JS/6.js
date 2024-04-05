e1d8bc81e8e7033135bb2e57033e24272d1628dd
zagorky <zagorkiynahyi@gmail.com>
2024-04-05 20:17:59 +0300

    задача 9

--- HW DOM/html/JS/6.js
+++ HW DOM/html/JS/6.js
@@ -1,3 +1,56 @@
 // Создайте блок с картинкой. Она должна меняться 
 // по таймеру каждую секунду. Добавьте 5 картинок, 
 // они будут меняться «по кругу».
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let str = '';
+// let count = 1;
+// function loadImg(count){
+//     document.querySelector("img").src=`img/${count}.jpg`;
+// }
+
+// function countImg(img){
+// count = 1;
+// count++;
+// if (count>6) count = 1;
+// loadImg (count);
+// }
+// setInterval(countImg(),1000);
+
+// window.addEventListener("load", function(){
+    // let img = document.getElementsByClassName("img")[0];
+    
+// });
+
+// let count = 1;
+// document.querySelector(".back").onclick = function (){
+//     count--;
+//     if (count<1) count = 6;
+//     loadImg (count);
+// }
+// document.querySelector(".forward").onclick = function (){
+//         count++;
+//         if (count>6) count = 1;
+//         loadImg (count);
+//     }
+//     function loadImg(count){
+//         document.querySelector("img").src=`img/${count}.jpg`;
+//     }
--- HW DOM/html/JS/9.js
+++ HW DOM/html/JS/9.js
@@ -6,10 +6,19 @@
 let img = document.querySelector(".runImg");
 let timer = setInterval("run()", 1000);
 function run(){
-    maxTop = screen.availHeight-150;
-    maxLeft = screen.availWidth-200;
-    let top = Math.round(Math.random()*maxLeft);
-    let left =  Math.round(Math.random()*maxTop);
-    img.style.top =` ${top}px`;
-    img.style.left = `${left}px `;
+    maxLeft = screen.availWidth-400;
+    console.log(screen.availHeight);
+    maxTop = screen.availHeight-300;
+    let top = Math.round(Math.random() * maxTop);
+    let left =  Math.round(Math.random() * maxLeft);
+    img.style.top =`${top}px`;
+    img.style.left = `${left}px`;
+}
+
+img.onclick = function() {
+    clearInterval(timer);
+    img.remove();
+    let h = document.createElement("h1");
+    h.textContent = `Ваша скидка составляет ${Math.round(Math.random()*50)} %`;
+    document.body.append(h);
 }
