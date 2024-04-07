ad19fc8fec1ff5119446c9aff62d924d3775eec4
zagorky <zagorkiynahyi@gmail.com>
2024-04-07 11:44:07 +0300

    решена 6 задача

--- HW DOM/html/JS/6.js
+++ HW DOM/html/JS/6.js
@@ -2,55 +2,16 @@
 // по таймеру каждую секунду. Добавьте 5 картинок, 
 // они будут меняться «по кругу».
 
+let images = ['1', '2', '3', '4', '5']
+let i = 0;
+document.getElementById("imgId");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let str = '';
-// let count = 1;
-// function loadImg(count){
-//     document.querySelector("img").src=`img/${count}.jpg`;
-// }
-
-// function countImg(img){
-// count = 1;
-// count++;
-// if (count>6) count = 1;
-// loadImg (count);
-// }
-// setInterval(countImg(),1000);
-
-// window.addEventListener("load", function(){
-    // let img = document.getElementsByClassName("img")[0];
-    
-// });
-
-// let count = 1;
-// document.querySelector(".back").onclick = function (){
-//     count--;
-//     if (count<1) count = 6;
-//     loadImg (count);
-// }
-// document.querySelector(".forward").onclick = function (){
-//         count++;
-//         if (count>6) count = 1;
-//         loadImg (count);
-//     }
-//     function loadImg(count){
-//         document.querySelector("img").src=`img/${count}.jpg`;
-//     }
+function roundImg() {
+    i++
+    if (i >= images.length)
+        {
+            i = 0;
+        }
+        imgId.src = `img/${images[i]}.jpg`;
+}
+setInterval(roundImg,1000);
